430875d3cad9e37eb6d13e62bc9d72e626eb2beb
viviniko <viviniko.shop@gmail.com>
2018-02-08 12:14:36 +0800

    :trollface:

--- src/UrlrewriteRequest.php
+++ src/UrlrewriteRequest.php
@@ -6,25 +6,20 @@ use Viviniko\Urlrewrite\Facades\Urlrewrite;
 
 trait UrlrewriteRequest
 {
-    protected $rewriteMethods = [];
-
     public function callAction($method, $parameters)
     {
-        if (in_array($method, $this->rewriteMethods) && is_null($parameters[0])) {
-            $request = request();
-            $urlRewrite = Urlrewrite::findByRequestPath($request->path());
-            if (!$urlRewrite) {
-                abort(404);
-            }
-
-            $entity = Urlrewrite::resolveEntity($urlRewrite->entity_type, $urlRewrite->entity_id);
-            if (!$entity) {
-                throw new \Exception('Url Rewrite Resolve ' . $urlRewrite->entity_type . ' Error.');
-            }
-
-            $parameters[0] = $entity;
-        }
+        $this->resolveAction($method, $parameters);
 
         return call_user_func_array([$this, $method], $parameters);
+    }
+
+    private function resolveAction(&$method, &$parameters)
+    {
+        if (empty($parameters) || !is_null($parameters[0]))
+            return;
+
+        if ($urlRewrite = Urlrewrite::findByRequestPath(request()->path())) {
+            $parameters[0] = Urlrewrite::resolveEntity($urlRewrite->entity_type, $urlRewrite->entity_id);
+        }
     }
 }
